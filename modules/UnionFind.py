2c826ceb67604bd59dd16e47b9fb8adc83a1b297
nao2002_ <nao@naosv.com>
2025-12-14 13:31:03 +0900

    fix: 命名を具体性のある名称へと変更
    ・各変数/関数の名称の変更
    ・それに伴い、要素数へのアクセス用プロパティを作成

--- modules/UnionFind.py
+++ modules/UnionFind.py
@@ -1,56 +1,75 @@
 class UnionFind:
-    def __init__(self, length):
-        """
-        length: 配列長
-        """
-        self.length = length
-        self.par = [-1]*length
+    """
+    Union-Find木
     
-    def root(self, x):
-        """
-        x番目の要素の親を取得
-        x: index(0-indexed)
+    _parent_or_size[i] < 0 のとき、iは根であり、グループの大きさ * -1 を表す
+    _parent_or_size[i] >= 0 のとき、iの親ノードのindexを表す
+    """
 
-        return: 要素の親index(0-indexed)
+    def __init__(self, num_nodes):
         """
-        if self.par[x] < 0:
-            return x
-        else:
-            self.par[x] = self.root(self.par[x])
-            return self.par[x]
+        num_nodes: 要素数
+        """
+        self._num_nodes = num_nodes
+        # 各要素の親ノードのindexまたはグループの大きさを格納する配列
+        self._parent_or_size = [-1]*num_nodes
+
+    @property
+    def num_nodes(self):
+        """
+        要素数を取得 (read-only)
+        uf.num_nodesのようにアクセス可能
+
+        return: 要素数
+        """
+        return self._num_nodes
     
-    def unite(self, x, y):
+    def find_root(self, node_idx):
         """
-        x番目とy番目の所属するグループを合成
-        x: index(0-indexed)
-        y: index(0-indexed)
+        node_idx番目の要素の根を取得
+        node_idx: index(0-indexed)
+
+        return: 要素の根のindex(0-indexed)
+        """
+        if self._parent_or_size[node_idx] < 0:
+            return node_idx
+        else:
+            self._parent_or_size[node_idx] = self.find_root(self._parent_or_size[node_idx])
+            return self._parent_or_size[node_idx]
+    
+    def union(self, node_u, node_v):
+        """
+        node_u番目とnode_v番目の所属するグループを合成
+        node_u: index(0-indexed)
+        node_v: index(0-indexed)
 
         return: 合成後のグループのサイズ
         """
-        x,y = self.root(x), self.root(y)
-        if x == y:
-            return -self.par[x]
+        root_x,root_y = self.find_root(node_u), self.find_root(node_v)
+        if root_x == root_y:
+            return -self._parent_or_size[root_x]
+        
         # グループのサイズが大きい方を親にする
-        if self.par[x] > self.par[y]:
-            x,y = y,x
-        self.par[x] += self.par[y]
-        self.par[y] = x
-        return -self.par[x]
+        if self._parent_or_size[root_x] > self._parent_or_size[root_y]:
+            root_x,root_y = root_y,root_x
+        self._parent_or_size[root_x] += self._parent_or_size[root_y]
+        self._parent_or_size[root_y] = root_x
+
+        return -self._parent_or_size[root_x]
     
-    def size(self, x):
+    def get_group_size(self, node_idx):
         """
-        x番目の所属するグループのサイズを取得
-        x: index(0-indexed)
+        node_idx番目の所属するグループのサイズを取得
+        node_idx: index(0-indexed)
 
         return: グループのサイズ
         """
-        return -self.par[self.root(x)]
+        return -self._parent_or_size[self.find_root(node_idx)]
     
-    def parents(self):
+    def get_all_roots(self):
         """
-        全ての親のindexを取得 O(N)
+        全ての根のindexを取得 O(N)
 
-        return: 全ての親のindex(0-indexed)の配列
+        return: 全ての根のindex(0-indexed)の配列
         """
-        return [i for i in range(self.length) if self.par[i] < 0]
-
+        return [i for i in range(self._num_nodes) if self._parent_or_size[i] < 0]
