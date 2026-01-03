f6266cb4a1e340f797b3224c3fd02d4b9818bb39
nao2002_ <nao@naosv.com>
2026-01-03 20:45:43 +0900

    add: ファイル追加

---
+++ contest/abc439/a.py
@@ -0,0 +1,15 @@
+#abc439a
+import sys
+from collections import defaultdict
+from collections import deque
+import heapq
+import math
+# from sortedcontainers import SortedList, SortedDict, SortedSet
+try:
+    import pypyjit
+    pypyjit.set_param('max_unroll_recursion=-1')
+except ImportError:
+    pass
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+def input(): return (sys.stdin.readline()).rstrip()
