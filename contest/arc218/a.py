f1376a8ac0a16c30add9aa457e8d5d871d24b339
nao2002_ <nao@naosv.com>
2026-05-23 23:47:52 +0900

    ARC218 (unrated)

--- contest/arc218/a.py
+++ contest/arc218/a.py
@@ -15,3 +15,48 @@ sys.set_int_max_str_digits(0)
 
 def input(): return (sys.stdin.readline()).rstrip()
 
+N,M = map(int,input().split())
+
+A = [list(map(int,input().split())) for _ in range(N)]
+
+seen = {}
+same = 0
+MOD = 998244353
+mul = [1]
+
+alls = set()
+for i in range(N):
+    alls |= set(A[i])
+
+for i in range(1,N+1):
+    mul.append(mul[-1]*M%MOD)
+
+others = {}
+for i in range(N):
+    included = defaultdict(int)
+    for j in range(M):
+        included[A[i][j]] += 1
+
+    for num in alls:
+        if not num in others:
+            others[num] = [N-included[num]]
+        else:
+            others[num].append((others[num]-1)*(N-included[num]))
+
+for i in range(N):
+    included = defaultdict(int)
+    for j in range(M):
+        included[A[i][j]] += 1
+    
+    for key, value in included.items():
+        if not key in seen:
+            seen[key] = [value,1,0,i]
+        else:
+            lastCount, oneshotValue, others, lastN = seen[key]
+            same += ((value * lastCount * oneshotValue * mul[i-(lastN+1)]) % MOD + (others * mul[i-(lastN+1)]) % MOD) % MOD
+            seen[key][0] = value
+            seen[key][1] = (lastCount * oneshotValue) % MOD
+            seen[key][2] = (max(1,seen[key][2])*(M-value))%MOD
+            seen[key][3] = i
+
+print((mul[N]*N-same)%MOD)
--- contest/arc218/b.py
+++ contest/arc218/b.py
@@ -15,3 +15,49 @@ sys.set_int_max_str_digits(0)
 
 def input(): return (sys.stdin.readline()).rstrip()
 
+T = int(input())
+
+user = ["Alice","Bob"]
+for _ in range(T):
+    N = int(input())
+    A = list(map(int,input().split()))
+    A.sort()
+
+    runlength = []
+
+    last = A[0]
+    cont = 1
+    for i in range(1,len(A)):
+        if A[i] == last:
+            cont += 1
+        else:
+            runlength.append((last, cont))
+            last = A[i]
+            cont = 1
+    else:
+        runlength.append((last, cont))
+
+    # print(runlength)
+    if runlength[0][0] >= 2:
+        print(user[0])
+        continue
+
+    current = runlength[0][0] % 2
+
+    if runlength[0][1] >= 2:
+        print(user[current])
+        continue
+
+    last = runlength[0][0]
+    winner = current
+    for i in range(1,len(runlength)):
+        if runlength[i][0] - last >= 2:
+            current ^= 1
+            winner = current
+            break
+        elif runlength[i][1] >= 2:
+            winner = current
+            break
+        last = runlength[i][0]
+    
+    print(user[winner])
