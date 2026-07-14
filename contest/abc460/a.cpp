faa7722043d73c79f27c09fb0961d89ac1c063a2
nao2002_ <nao@naosv.com>
2026-07-15 02:25:30 +0900

    ABC460 (unrated)

--- contest/abc460/a.cpp
+++ contest/abc460/a.cpp
@@ -8,6 +8,16 @@
 
 int main()
 {
-    
+    int N,M;
+    std::cin >> N >> M;
+
+    int ans = 0;
+
+    while (M != 0) {
+        M = N % M;
+        ans += 1;
+    }
+
+    std::cout << ans << std::endl;
     return 0;
 }
--- contest/abc460/c.cpp
+++ contest/abc460/c.cpp
@@ -5,9 +5,40 @@
 #include <map>
 #include <unordered_set>
 #include <unordered_map>
+#include <set>
 
 int main()
 {
-    
+    int N,M;
+    std::cin >> N >> M;
+    std::vector<int> A(N);
+    std::vector<int> B(M);
+
+    for (int i=0; i<N;i++) {
+        std::cin >> A[i];
+    }
+    for (int i=0; i<M;i++) {
+        std::cin >> B[i];
+    }
+    std::sort(A.begin(),A.end());
+    std::sort(B.begin(),B.end());
+
+    int idxA,idxB;
+    idxA = N-1;
+    idxB = M-1;
+
+    int ans = 0;
+    while (idxA >= 0 && idxB >= 0) {
+        if (B[idxB] <= A[idxA]*2) {
+            ans += 1;
+            --idxA;
+            --idxB;
+        } else {
+            --idxB;
+        }
+    }
+
+    std::cout << ans << std::endl;
+
     return 0;
 }
